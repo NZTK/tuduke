77eb2409b00c1651dc3d1e43781b5ec1da00cc67
NZTK <tkm.2348nishizawa@gmail.com>
2019-07-09 12:46:01 +0000

    A

---
+++ db/migrate/20190709120555_remove_id_from_relationship.rb
@@ -0,0 +1,6 @@
+class RemoveIdFromRelationship < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :relationships, :follower_id, :integer
+    remove_column :relationships, :followed_id, :integer
+  end
+end
